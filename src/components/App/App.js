f804b3867820cb8f0c684d4c780155b267fac74f
Tim Dugan <tadugan@me.com>
2021-10-29 16:11:15 -0500

    added App folder in components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { logHello } from '../../utils';
 
 function App() {
     return (
         <div className='App'>
             <h2>X-wing Maneuver Selector</h2>
+            {logHello()}
         </div>
     );
 }
