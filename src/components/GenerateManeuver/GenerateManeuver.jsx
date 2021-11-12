eb5349df8ea9127a9d7c1a7ff339fea9a2f619c8
Tim Dugan <tadugan@me.com>
2021-11-12 15:39:51 -0600

    Experimenting with JS functions to convert CSV maneuver data into a JS object

--- src/components/GenerateManeuver/GenerateManeuver.jsx
+++ src/components/GenerateManeuver/GenerateManeuver.jsx
@@ -1,10 +1,11 @@
 import { Button } from '@material-ui/core';
+import { generateManeuver } from '../../utils';
 import './GenerateManeuver.css';
 
 function GenerateManeuver() {
     return (
         <div className='generate-maneuver'>
-            <Button className='generate-maneuver-button' variant='contained' color='primary'>Generate Maneuver</Button>
+            <Button className='generate-maneuver-button' onClick={() => generateManeuver()} variant='contained' color='primary'>Generate Maneuver</Button>
             <h3 className='generate-maneuver-display'>Maneuver: </h3>
         </div>
     );
